e21e532d266610480ae30def7eecb049c9f56a0b
Troy Pavlek <ebonwumon@depotwarehouse.net>
2014-10-29 12:40:46 -0600

    Added configuration

---
+++ config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'ldap' => [
+        'host' => 'ldaps://directory.srv.ualberta.ca',
+        'port' => 636,
+        'service_username' => '',
+        'service_password' => ''
+    ]
+];
