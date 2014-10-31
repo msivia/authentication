4ad7c41562a5ecfb8609c0676cb1f16bc086a5fe
Troy Pavlek <ebonwumon@depotwarehouse.net>
2014-10-31 11:28:05 -0600

    Updated service provider config

--- src/FrameworkIntegration/AuthenticationServiceProvider.php
+++ src/FrameworkIntegration/AuthenticationServiceProvider.php
@@ -27,13 +27,14 @@ class AuthenticationServiceProvider extends ServiceProvider {
 
     public function boot() {
         $this->package('u-alberta/authentication');
-        $this->app->bind('\UAlberta\IST\Authentication\Configuration', function() {
+        $ldap_config = \Config::get('authentication::ldap');
+        $this->app->bind('\UAlberta\IST\Authentication\Configuration', function() use ($ldap_config) {
             $configuration = new Configuration();
             $configuration->setLDAP(
-                \Config::get('authentication::ldap.host'),
-                \Config::get('authentication::ldap.port'),
-                \Config::get('authentication::ldap.service_username'),
-                \Config::get('authentication::ldap.service_port')
+                $ldap_config['host'],
+                $ldap_config['port'],
+                $ldap_config['service_username'],
+                $ldap_config['service_password']
             );
 
             return $configuration;
@@ -44,8 +45,8 @@ class AuthenticationServiceProvider extends ServiceProvider {
             '\UAlberta\IST\Authentication\Providers\LDAPUserProvider'
         );
 
-        $this->app->bind('\Dreamscapes\Ldap\Core\LinkResource', function() {
-            $resource = new LinkResource(\Config::get('authentication::ldap.host') . ":" . \Config::get('authentication::ldap.port'));
+        $this->app->bind('\Dreamscapes\Ldap\Core\LinkResource', function() use ($ldap_config) {
+            $resource = new LinkResource("{$ldap_config['host']}:{$ldap_config['port']}");
             return $resource;
         });
     }
