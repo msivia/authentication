018d6b2c20647cfb26ab7de60d9aad3598f2aba1
msivia <msivia@ualberta.ca>
2015-08-31 16:27:57 -0700

    LDAP service credentials exception added

---
+++ src/Exceptions/CredentialsNotFoundException.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: msivia
+ * Date: 8/31/15
+ * Time: 3:49 PM
+ */
+
+namespace UAlberta\IST\Authentication\Exceptions;
+
+
+use Exception;
+
+class CredentialsNotFoundException extends Exception
+{
+    protected $case;
+
+    /**
+     * Creates a new exception.
+     *
+     * @param string $identifier expects CCID to be searched in LDAP
+     * @param int $case
+     */
+    public function __construct($identifier, $case)
+    {
+        $this->case = $case;
+
+        // Pass the message  to the parent
+        $message = "User {$identifier} could not be looked in LDAP system:";
+        switch ($this->case) {
+            case 1:
+                $message = "{$message} Missing username for LDAP System";
+                break;
+            case 2:
+                $message = "{$message} Missing password for LDAP System";
+                break;
+            case 3:
+                $message = "{$message} Missing username & password for LDAP System";
+                break;
+            default:
+                $message = "{$message} Missing credentials";
+        }
+
+        parent::__construct((string)$message);
+
+    }
+}
