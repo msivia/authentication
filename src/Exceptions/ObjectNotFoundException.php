62008391b0b240c27526b6577a1aa8735f074663
msivia <msivia@ualberta.ca>
2015-08-31 10:41:38 -0700

    Added exception ObjectNotFound

---
+++ src/Exceptions/ObjectNotFoundException.php
@@ -0,0 +1,20 @@
+<?php
+namespace UAlberta\IST\Authentication;
+
+use Exception;
+
+class ObjectNotFoundException extends Exception
+{
+    /**
+     * Creates a new exception.
+     *
+     * @param string $identifier expects CCID for approved user
+     */
+    public function __construct($identifier)
+    {
+        // Pass the message and integer code to the parent
+        $message = "Could not find user: {$identifier} in the LDAP system";
+        parent::__construct((string)$message);
+
+    }
+}
